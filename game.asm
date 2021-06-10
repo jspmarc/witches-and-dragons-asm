df70e4f942498c13383b30e83b42541ebe9f622a
Josep Marcello <jspmarcello@live.com>
2021-06-11 04:05:03 +0700

    add README

--- game.asm
+++ game.asm
@@ -1,28 +1,5 @@
-; # Game RPG turn-based sederhana
-; > [Game Inspiration](https://www.youtube.com/watch?v=6izF5Wyr94o&t=44s)\
-; ---- Author ----
 ; Josep Marcello (jspmarcello@live.com)
 ; 11th June 2021
-; ---- End Author ----
-; ## Aturan Permainan
-; ### Pemain
-; Seoranng pemain memiliki seorang karakter
-; ### Sebuah karakter dapat melakukan:
-; - Serangan "lemah" (1 stamina)\
-;   Memberikan 2 HP damage
-; - Berobat (2 stamina)\
-;   "Mengembalikan" 2 HP
-; - Serangan "kuat" (3 stamina)\
-;   Memberikan 5 HP damage
-; ### Kondisi awal:
-; Setiap karakter memiliki 100 stamina dan 100 HP di awal
-; ### Stamina:
-; - Ketika stamina habis, karakter ga bisa ngapa-ngapain lagi dan turn di-skip
-; - 2 Stamina akan di-restore setiap 3 turn
-; ### Kondisi menang/kalah:
-; - Pemain dengan HP terendah kalah
-; - Permainan berakhir ketika kedua karakter sudah tidak ada stamina
-; - Permainan berakhir ketika salah seorang karakter kehabisan HP
 
 section .text:
 
