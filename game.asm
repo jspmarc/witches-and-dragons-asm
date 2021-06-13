988b0ea6f082154988fb3c4826b4b4c8494421e1
Josep Marcello <jspmarcello@live.com>
2021-06-13 23:01:35 +0700

    feat: gameInit and readnput

--- game.asm
+++ game.asm
@@ -3,16 +3,36 @@
 
 ; ---- DATA SECTION ----
 section .data
-hello: db "Hello, world!", 0xA
-helloLen equ $-hello
+gameTitle: db "[---- Witches 'n Dragons ----]"
 
-firstCharName: db "V"
-firstCharNameLen equ $-firstCharName
-secondCharName: db "Panam"
-secondCharNameLen equ $-secondCharName
+p1Name: db "Witch"
+p1NameLen: equ $-p1Name
+p2Name: db "Dragon"
+p2NameLen: equ $-p2Name
 
-minusSign: db "-"
-minusSignLen: equ $-minusSign
+playerTurn1: db "It's the "
+playerTurn1Len: equ $-playerTurn1
+playerTurn2: db "'s turn.", 10
+playerTurn2Len: equ $-playerTurn2
+askPlayerAction: db "What do you want to do?", 10,\
+				"[1]: weak attack (1 stamina)", 10,\
+				"[2]: strong attack (3 stamina)", 10,\
+				"[3]: heal (2 stamina)", 10,\
+				"[4]: Stats", 10,\
+				"[5]: Help", 10,\
+				"[6]: Quit game", 10,\
+				"(1/2/3/4/5/6)> "
+askPlayerActionLen: equ $-askPlayerAction
+
+badInputMsg: db "Your input is invalid, please input between 1 to 6 (inclusive.)", 10
+badInputMsgLen: equ $-badInputMsg
+
+dbgMsg1: db "Halo", 10
+dbgMsg1Len: equ $-dbgMsg1
+dbgMsg2: db "Aku di sini", 10
+dbgMsg2Len: equ $-dbgMsg1
+dbgMsg3: db "Tangkap aku, kawan!", 10
+dbgMsg3Len: equ $-dbgMsg1
 
 ; ---- TEXT SECTION ----
 section .text
@@ -20,17 +40,109 @@ section .text
 global _start
 
 _start:
-	mov rdi, -123499999
-	call printNum
-	call printNewLine
+	; Tabel variabel
+	; Nama          | register
+	; --------------|----------
+	; p1Stammina    | r8
+	; p1Hp          | r9
+	; p2Stamina     | r10
+	; p2Hp          | r11
+	; roundCounter  | r12
+	; isP1Winner    | r13
+	; p1Input       | r14, int
+	; p12nput       | r15, int
 
+	.gameInit:
+		mov r8, 100
+		mov r9, 100
+		mov r10, 100
+		mov r11, 100
+		xor r12, r12
+		xor r13, r13
+
+	.startTurn:
+		inc r12
+		mov rax, r12
+		and rax, 0xFF
+
+		.playerTurn1:
+			mov rax, 1
+			mov rdi, 1
+			mov rsi, playerTurn1
+			mov rdx, playerTurn1Len
+			syscall
+
+			test rax, rax
+			jnz .writeP1Name ; genap
+			jmp .writeP2Name ; ganjil
+
+		.playerTurn2:
+			mov rax, 1
+			mov rdi, 1
+			mov rsi, playerTurn2
+			mov rdx, playerTurn2Len
+			syscall
+
+		.playerInput:
+			mov rax, 1
+			mov rdi, 1
+			mov rsi, askPlayerAction
+			mov rdx, askPlayerActionLen
+			syscall
+			call getNumInput
+			cmp rax, 1
+			jl .badInput
+			cmp rax, 6
+			jg .badInput
+
+			xor rdi, rdi
+			call exit
+
+	.endTurn:
+		mov rdi, 1
+		call printNum
+		call printNewLine
+
+	.gameEnd:
+		mov rdi, 1
+		call printNum
+		call printNewLine
+
+	xor rdi, rdi
 	call exit
 
-; void exit();
-; fungsi untuk keluar dari program dengan exit code 0
+	.writeP1Name:
+		mov rax, 1
+		mov rdi, 1
+		mov rsi, p1Name
+		mov rdx, p1NameLen
+		syscall
+		jmp .playerTurn2
+
+	; void writeP2Name();
+	; Menuliskan nama pemain 2 tanpa new line
+	.writeP2Name:
+		mov rax, 1
+		mov rdi, 1
+		mov rsi, p2Name
+		mov rdx, p2NameLen
+		syscall
+		jmp .playerTurn2
+
+	.badInput:
+		mov rax, 1
+		mov rdi, 1
+		mov rsi, badInputMsg
+		mov rdx, badInputMsgLen
+		syscall
+		jmp .playerInput
+
+
+; *** Utility functions ***
+; void exit(int code);
+; fungsi untuk keluar dari program dengan exit code `code`
 exit:
 	mov rax, 60			; exit syscall
-	xor rdi, rdi
 	syscall
 
 ; void printNum(long num);
@@ -147,6 +259,54 @@ getNumInput:
 
 		cmp rdx, rcx ; kondisi, banyak yang "dibaca" == banyak digit
 		jne .loop ; if (i != rcx) goto loop;
+
+	mov rsp, rbp
+	pop rbp
+	ret
+
+; void debug1()
+; nulisin pesan debug
+debug1:
+	push rbp
+	mov rbp, rsp
+
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, dbgMsg1
+	mov rdx, dbgMsg1Len
+	syscall
+
+	mov rsp, rbp
+	pop rbp
+	ret
+
+; void debug2()
+; nulisin pesan debug
+debug2:
+	push rbp
+	mov rbp, rsp
+
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, dbgMsg2
+	mov rdx, dbgMsg2Len
+	syscall
+
+	mov rsp, rbp
+	pop rbp
+	ret
+
+; void debug3()
+; nulisin pesan debug
+debug3:
+	push rbp
+	mov rbp, rsp
+
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, dbgMsg3
+	mov rdx, dbgMsg3Len
+	syscall
 
 	mov rsp, rbp
 	pop rbp
